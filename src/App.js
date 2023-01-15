549cff414b9946c386d3ee965aa77084fb30b304
William <williamw15@protonmail.com>
2023-01-14 22:19:18 -0500

    add background color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+/** @format */
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Header from './components/Header/Header';
 
+// #858585 - light grey
+
 function App() {
   return (
-    <div>
+    <div
+      style={{ backgroundColor: '#4d4d4d', height: '100vh', width: '100vw' }}
+    >
       <Header />
     </div>
   );
