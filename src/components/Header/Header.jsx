7ef85ba3dfa0d99abc2466959b6fb1b8ceb381b0
William <williamw15@protonmail.com>
2023-01-14 22:18:44 -0500

    header and header styling laid out

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,60 @@
 
 import React from 'react';
 
+import './Header.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 export default class Header extends React.Component {
   render() {
     return (
       <div>
-        <nav className='navbar navbar-expand-lg navbar-light bg-primary'>
-          <a
-            className='navbar-brand'
-            href='#'
-          >
-            Navbar
-          </a>
+        <nav className='navbar navbar-expand navbar-dark bg-dark'>
+          <div className='container-fluid navbar-container'>
+            <a
+              className='navbar-brand text-info'
+              href='#'
+            >
+              <img
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png?20220125121207'
+                alt=''
+                height='28'
+                width='35'
+              />
+              <img
+                src='https://seeklogo.com/images/B/bootstrap-logo-3C30FB2A16-seeklogo.com.png'
+                alt=''
+                height='32'
+                width='35'
+              />
+              <strong>WILLIAM WATSON</strong>
+            </a>
+            <div className='collapse navbar-collapse justify-content-end'>
+              <div className='navbar-nav'>
+                <a
+                  className='nav-link text-info'
+                  href='#'
+                >
+                  About
+                </a>
+              </div>
+              <div className='navbar-nav'>
+                <a
+                  className='nav-link text-info'
+                  href='#'
+                >
+                  Portfolio
+                </a>
+              </div>
+              <div className='navbar-nav'>
+                <a
+                  className='nav-link text-info'
+                  href='#'
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
         </nav>
       </div>
     );
